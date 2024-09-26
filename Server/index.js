31624625a1a3a3820d509ee9c8a39653abc1d92c
PraDeePKuMaR <pradeepkumar24rk@gmail.com>
2024-09-26 15:44:52 +0530

    GridFs for profilePic is added

--- Server/index.js
+++ Server/index.js
@@ -1,48 +1,54 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors=require('cors');
+const cors = require("cors");
 const dotenv = require("dotenv");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const http = require("http");
-const path = require('path');
+const path = require("path");
+const { GridFSBucket } = require("mongodb");
 
 dotenv.config();
 const app = express();
 app.use(cookieParser());
 app.use(cors());
-app.use(express.json({limit:"3mb"}));
+app.use(express.json({ limit: "3mb" }));
 const server = http.createServer(app);
+let bucket;
 
 //Routers
-const authRoute=require("./routers/auth");
-const postRoute=require("./routers/post");
-const followRoute=require("./routers/follow");
-const userRoute=require("./routers/userRoutes");
+const authRoute = require("./routers/auth");
+const postRoute = require("./routers/post");
+const followRoute = require("./routers/follow");
+const userRoute = require("./routers/userRoutes");
 const socketHandler = require("./routers/socket");
 
 //connect the DB
 mongoose
-.connect(process.env.MONGO_URL||"")
-.then(() => {
-  console.log("DB is connected");
-})
-.catch((err) => {
-  console.error(err)
-});
+  .connect(process.env.MONGO_URL || "")
+  .then((conn) => {
+    console.log("DB is connected");
+    const db = conn.connection.db;
+    bucket = new GridFSBucket(db, { bucketName: "photos" });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 //Endpoint call
-app.use('/auth',authRoute);
-app.use('/post',postRoute);
-app.use('/follow',followRoute);
-app.use('/user',userRoute);
+app.use("/auth", authRoute);
+app.use("/post", postRoute);
+app.use("/follow", followRoute);
+app.use("/user", userRoute);
 
-app.use(express.static(path.join(__dirname, 'Client/build')));
 // Catch-all route to serve the React app (BrowserRouter handles client-side routing)
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+app.use(express.static(path.join(__dirname, "Client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
+// Message Service
 socketHandler(server);
+
 server.listen(5010, () => {
   console.log("Server is running");
 });
--- Server/routers/userRoutes.js
+++ Server/routers/userRoutes.js
@@ -4,6 +4,10 @@ const CryptoJS = require("crypto-js");
 const {TokenVerify,verifyTokenAndAuthorization} = require("./verifyToken");
 const nodemailer = require("nodemailer");
 const otpGenerator = require("otp-generator");
+const mongoose = require("mongoose");
+const upload = require('../Middleware/ImageStorageMiddleware.js')
+const { GridFSBucket } = require('mongodb');
+
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -15,6 +19,50 @@ const transporter = nodemailer.createTransport({
     pass: process.env.MAIL_PASSWORD,
   },
   tls: { rejectUnauthorized: false },
+});
+
+// User Image Upload Endpoint
+router.post('/profilePicUpload', upload.single('file'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
+  }
+  res.status(200).json({ message: 'File uploaded successfully', imageUrl:`http://localhost:5010/user/profilePic/${req.file.filename}` });
+});
+
+// Get Profile Pic Endpoint
+router.get("/profilePic/:filename", async (req, res) => {
+  try {
+    // Ensure the connection is established before using GridFSBucket
+    const db = mongoose.connection.db;
+
+    // Initialize GridFSBucket
+    const bucket = new GridFSBucket(db, {
+      bucketName: 'photos' // Your GridFS collection name
+    });
+
+    // Find the file in the GridFS collection by filename
+    const files = await db.collection('photos.files').findOne({ filename: req.params.filename });
+    
+    if (!files) {
+      return res.status(404).json({ message: "File not found" });
+    }
+
+    // Create a readable stream from the GridFSBucket
+    const readStream = bucket.openDownloadStreamByName(req.params.filename);
+
+    // Pipe the readable stream to the response
+    readStream.pipe(res);
+
+    // Handle errors on the stream
+    readStream.on('error', (err) => {
+      console.log(err);
+      res.status(500).send("An error occurred while streaming the image.");
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while fetching the image");
+  }
 });
 
 //Update User profile
